16903a8309b55a92b92b70beba07324e06ccad3f
yttrium285002 <yyh1921@163.com>
2020-01-31 00:07:04 +0800

    20200131

---
+++ 20.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+w = 20
+while w > 1:
+    print(w)
+    w -= 1
---
+++ testhundred.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+number = int(input("Enter an integer: "))
+if number <= 100:
+    print("your number is less than or equal to 100")
+else:
+    print("your number is greater than 100")
